a891be5699c2e6976833ac6108d0b3a8a4dfa728
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-05-31 17:28:41 -0400

    Adding HTTPMethod type.

---
+++ src/types/HTTPMethod.ts
@@ -0,0 +1,10 @@
+//
+// Type
+//
+
+/**
+ * An HTTP method.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ */
+export type HTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
