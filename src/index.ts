48448a0d28d7d0b0c34bfcfaafe34a07fdbda855
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-12-24 10:56:00 -0500

    refactor: removed "Middleware" from middleware file names

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ export * from "./classes/FritterFile.js";
 export * from "./classes/FritterRequest.js";
 export * from "./classes/FritterResponse.js";
 
-export * as BodyParserMiddleware from "./middlewares/BodyParserMiddleware.js";
-export * as CorsMiddleware from "./middlewares/CorsMiddleware.js";
-export * as CurrentPageNumberMiddleware from "./middlewares/CurrentPageNumberMiddleware.js";
-export * as ForceSslMiddleware from "./middlewares/ForceSslMiddleware.js";
-export * as LogRequestMiddleware from "./middlewares/LogRequestMiddleware.js";
-export * as RouterMiddleware from "./middlewares/RouterMiddleware.js";
-export * as SameOriginFrameMiddleware from "./middlewares/SameOriginFrameMiddleware.js";
-export * as StaticMiddleware from "./middlewares/StaticMiddleware.js";
+export * as BodyParserMiddleware from "./middlewares/BodyParser.js";
+export * as CorsMiddleware from "./middlewares/Cors.js";
+export * as CurrentPageNumberMiddleware from "./middlewares/CurrentPageNumber.js";
+export * as ForceSslMiddleware from "./middlewares/ForceSsl.js";
+export * as LogRequestMiddleware from "./middlewares/LogRequest.js";
+export * as RouterMiddleware from "./middlewares/Router.js";
+export * as SameOriginFrameMiddleware from "./middlewares/SameOriginFrame.js";
+export * as StaticMiddleware from "./middlewares/Static.js";
 
 export * from "./types/HTTPMethod.js";
 export * from "./types/MiddlewareFunction.js";
